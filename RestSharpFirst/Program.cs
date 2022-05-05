1dd4e9b211f60b88e28e3c96bea3db9f924905c2
voidnire <nirerin15@gmail.com>
2022-05-05 11:19:52 -0400

    Adicionar arquivos de projeto.

---
+++ RestSharpFirst/Program.cs
@@ -0,0 +1,24 @@
+﻿using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace RestSharpFirst
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string baseurl = "https://jsonplaceholder.typicode.com/todos";
+
+            var client = new RestClient(baseurl);
+
+            var req = new RestRequest();
+            req.AddParameter("id", "1");
+            var resp = await client.GetAsync(req);
+
+            Console.WriteLine(resp.Content.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
